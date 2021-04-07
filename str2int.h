64b0a231d12530395eb4ee76bb31aad661bcd644
Chandan O. Aggarwal <f004rqs@plank.thayer.dartmouth.edu>
2021-04-07 15:36:01 -0400

    Number 3.

---
+++ str2int.h
@@ -0,0 +1,19 @@
+/* ***************** str2int ********************** */
+/*                                                                                                                               
+ * Convert a string to an integer, returning that integer.                                                                       
+ * Returns true if successful, or false if any error.                                                                            
+ * It is an error if there is any additional character beyond the integer.                                                        * Assumes number is a valid pointer.                                                                                            
+ */
+
+#ifndef __STR2INT_H__
+#define __STR2INT_H__
+
+#include <stdbool.h>
+
+// The following is one of my favorite tricks.                                                                                 
+// We use sscanf() to parse a number, expecting there to be no following                                                       
+// character ... but if there is, the input is invalid.                                                                        
+// For example, 1234x will be invalid, as would 12.34 or just x. 
+extern bool str2int(const char string[], int * number);
+
+#endif //  __READLINE_H__
